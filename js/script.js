4516ee722a05f5057718ab909baa43e36002a22a
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-05-19 22:02:54 -0300

    section 'benefits' completed

--- js/script.js
+++ js/script.js
@@ -2,17 +2,19 @@
 const slideButtons = [...document.querySelectorAll('.js-buttons')];
 const slideContent = [...document.querySelectorAll('.js-slide')];
 
-slideContent[0].classList.add('active');
-slideButtons[0].classList.add('active');
+if (slideButtons.length && slideContent.length) {
+    slideContent[0].classList.add('active');
+    slideButtons[0].classList.add('active');
 
-const showContent = (index) => {
-    slideContent.forEach((content) => content.classList.remove('active'));
-    slideContent[index].classList.add('active');
+    const showContent = (index) => {
+        slideContent.forEach((content) => content.classList.remove('active'));
+        slideContent[index].classList.add('active');
 
-    slideButtons.forEach((content) => content.classList.remove('active'));
-    slideButtons[index].classList.add('active');
-};
+        slideButtons.forEach((content) => content.classList.remove('active'));
+        slideButtons[index].classList.add('active');
+    };
 
-slideButtons.forEach((button, index) => {
-    button.addEventListener('click', () => showContent(index));
-})
+    slideButtons.forEach((button, index) => {
+        button.addEventListener('click', () => showContent(index));
+    })
+}
