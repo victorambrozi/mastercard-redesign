aa7ff492df7c301b750e6acf95af380b6845c1a2
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-05-20 08:50:46 -0300

    animation search

--- js/script.js
+++ js/script.js
@@ -1,20 +1,40 @@
+window.addEventListener('load', start);
 
-const slideButtons = [...document.querySelectorAll('.js-buttons')];
-const slideContent = [...document.querySelectorAll('.js-slide')];
 
-if (slideButtons.length && slideContent.length) {
-    slideContent[0].classList.add('active');
-    slideButtons[0].classList.add('active');
+function initSlide() {
+    const slideButtons = [...document.querySelectorAll('.js-buttons')];
+    const slideContent = [...document.querySelectorAll('.js-slide')];
 
-    const showContent = (index) => {
-        slideContent.forEach((content) => content.classList.remove('active'));
-        slideContent[index].classList.add('active');
+    if (slideButtons.length && slideContent.length) {
+        slideContent[0].classList.add('active');
+        slideButtons[0].classList.add('active');
 
-        slideButtons.forEach((content) => content.classList.remove('active'));
-        slideButtons[index].classList.add('active');
+        const showContent = (index) => {
+            slideContent.forEach((content) => content.classList.remove('active'));
+            slideContent[index].classList.add('active');
+
+            slideButtons.forEach((content) => content.classList.remove('active'));
+            slideButtons[index].classList.add('active');
+        };
+
+        slideButtons.forEach((button, index) => {
+            button.addEventListener('click', () => showContent(index));
+        });
     };
+};
 
-    slideButtons.forEach((button, index) => {
-        button.addEventListener('click', () => showContent(index));
-    })
-}
+function animateSearch() {
+    const inputSearch = document.getElementById('search');
+    const search = document.querySelector('.js-search');
+
+    const showInput = () => {
+        inputSearch.value = '';
+        inputSearch.classList.toggle('active');
+    }
+    search.addEventListener('click', showInput);
+};
+
+function start() {
+    initSlide();
+    animateSearch();
+};
