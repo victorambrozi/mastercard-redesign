096d541addfb5e03080038f235a4f8fb2a521d5d
Victor Ambrozi <victorambrozi.dev@gmail.com>
2021-06-01 15:37:48 -0300

    animaçao scroll (resolver bug)

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 window.addEventListener('load', start);
 
+const slideButtons = [...document.querySelectorAll('.js-buttons')];
+const slideContent = [...document.querySelectorAll('.js-slide')];
+
 
 function initSlide() {
-    const slideButtons = [...document.querySelectorAll('.js-buttons')];
-    const slideContent = [...document.querySelectorAll('.js-slide')];
+    slideContent[0].classList.add('active');
+    slideButtons[0].classList.add('active');
 
     if (slideButtons.length && slideContent.length) {
-        slideContent[0].classList.add('active');
-        slideButtons[0].classList.add('active');
-
         const showContent = (index) => {
             slideContent.forEach((content) => content.classList.remove('active'));
             slideContent[index].classList.add('active');
@@ -47,18 +47,44 @@ function animateArrow() {
     const fieldArrows = [...document.querySelectorAll('.js-animation-arrow')];
     const arrows = [...document.querySelectorAll('.js-arrow')];
 
-    const activeArrow = (index) => {
-        arrows.forEach((arrow) => arrow.classList.remove('active'));
-        arrows[index].classList.add('active');
+    if (fieldArrows.length && arrows.length) {
+        const activeArrow = (index) => {
+            arrows.forEach((arrow) => arrow.classList.remove('active'));
+            arrows[index].classList.add('active');
+        }
+
+        fieldArrows.forEach((field, index) => {
+            field.addEventListener('mouseover', () => activeArrow(index));
+        })
+    }
+}
+
+function animateScroll() {
+    const sections = [...document.querySelectorAll('.js-scroll')];
+
+    const getHeight = () => {
+        sections.forEach((section) => {
+            const height = section.getBoundingClientRect().top;
+            const halfScreen = window.innerHeight * 0.6;
+            const isHalf = height < halfScreen;
+
+            if (isHalf) {
+                slideContent[0].classList.add('active');
+                slideButtons[0].classList.add('active');
+                section.classList.add('active');
+            } else {
+                section.classList.remove('active');
+            }
+        })
     }
 
-    fieldArrows.forEach((field, index) => {
-        field.addEventListener('mouseover', () => activeArrow(index))
-    })
+    window.addEventListener('scroll', getHeight);
+    getHeight();
 }
 
 function start() {
     initSlide();
     animateSearch();
     animateArrow();
+    animateScroll();
 };
